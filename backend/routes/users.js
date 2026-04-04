a5fe251e9da9a583eb1942658dd166d100d43e28
hgrele <helene.grele@theodo.fr>
2026-04-04 13:47:13 +0200

    feat: ✨ back: create routes to sign up and to login

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,4 +1,6 @@
+import bcrypt from 'bcrypt';
 import express from 'express';
+import jwt from 'jsonwebtoken';
 import { appDataSource } from '../datasource.js';
 import User from '../entities/user.js';
 
@@ -35,6 +37,112 @@ router.post('/new', function (req, res) {
       } else {
         res.status(500).json({ message: 'Error while creating the user' });
       }
+    });
+});
+
+/**
+ * @swagger
+ * /api/users/signup:
+ *   post:
+ *     summary: Créer un compte utilisateur
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - firstname
+ *               - lastname
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *               firstname:
+ *                 type: string
+ *               lastname:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Utilisateur créé avec succès
+ *       500:
+ *         description: L'utilisateur existe déjà
+ */
+router.post('/signup', function (req, res) {
+  const { email, firstname, lastname, password } = req.body;
+  const userRepository = appDataSource.getRepository(User);
+
+  bcrypt.hash(password, 10).then(function (hash) {
+    const newUser = userRepository.create({ email, firstname, lastname, hash });
+    userRepository
+      .insert(newUser)
+      .then(function () {
+        res.status(201).json({ message: 'User created successfully' });
+      })
+      .catch(function (error) {
+        console.error(error);
+        res.status(500).json({ message: 'User already exists' });
+      });
+  });
+});
+
+/**
+ * @swagger
+ * /api/users/login:
+ *   post:
+ *     summary: Connexion d'un utilisateur
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Connexion réussie, retourne un token JWT
+ *       401:
+ *         description: Email ou mot de passe incorrect
+ *       500:
+ *         description: Erreur serveur
+ */
+router.post('/login', function (req, res) {
+  const { email, password } = req.body;
+  const userRepository = appDataSource.getRepository(User);
+
+  userRepository
+    .findOneBy({ email })
+    .then(function (user) {
+      if (!user) {
+        return res
+          .status(401)
+          .json({ message: 'Email ou mot de passe incorrect' });
+      }
+      bcrypt.compare(password, user.hash).then(function (match) {
+        if (!match) {
+          return res
+            .status(401)
+            .json({ message: 'Email ou mot de passe incorrect' });
+        }
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+          expiresIn: '1h',
+        });
+        res.status(200).json({ token });
+      });
+    })
+    .catch(function (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Erreur serveur' });
     });
 });
 
