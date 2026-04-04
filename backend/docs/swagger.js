cbfe01ed74f355e2efae1650b5697e7459044b16
hgrele <helene.grele@theodo.fr>
2026-04-04 13:42:50 +0200

    feat: ✨ back: add swager config file

---
+++ backend/docs/swagger.js
@@ -0,0 +1,15 @@
+import swaggerJsdoc from 'swagger-jsdoc';
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'My movies API',
+      version: '1.0.0',
+      description: 'API REST pour la gestion des utilisateurs et des films',
+    },
+  },
+  apis: ['./routes/*.js'],
+};
+
+export const swaggerSpec = swaggerJsdoc(options);
